dd23895aa009f075c174948d0ba8890cbbf59b85
Abishek Aditya <abishekaditya@outlook.com>
2016-11-25 21:18:13 +0530

    Added username for registration

--- db/migrate/20161124045856_add_index_to_table_name.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToTableName < ActiveRecord::Migration[5.0]
-  def change
-    add_index :users, :username, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20161124050418) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "username"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
